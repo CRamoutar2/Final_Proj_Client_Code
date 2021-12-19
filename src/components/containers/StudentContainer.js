4e9e8b344c6b6229c1a20f373440045a5ba4b455
GuyotDavid <48332768+GuyotDavid@users.noreply.github.com>
2021-12-18 21:56:01 -0500

    Updates
    Refreshes after we take actions, redirects us back to main students page if necessary.

--- src/components/containers/StudentContainer.js
+++ src/components/containers/StudentContainer.js
@@ -1,18 +1,49 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchStudentThunk } from "../../store/thunks";
+import { fetchStudentThunk, deleteStudentThunk } from "../../store/thunks";
 import { StudentView } from "../views";
+import { Redirect} drom 'react-router-dom';
 
 class StudentContainer extends Component {
-  componentDidMount() {
+  Constructor(props) {
+    super(props);
+    this.state {
+      redirect: false;
+      showEdit: false;
+    }
+    
+    componentDidMount() {
     //getting student ID from url
     this.props.fetchStudent(this.props.match.params.id);
-  }
+    }
+    
+    handleClick = async () => {
+      await this.props.deleteStudent(this.props.match.params.id);
+      this.setState({
+        redirect: true
+      })
+    }
+    
+    showEdit = () => {
+      this.setState({
+        showEdit: !this.state.showEdit
+      })
+    }
+    
+    refresh = async () => {
+      await this.props.fetchStudent(this.props.match.params.id);
+    }
 
-  render() {
-    return (
-      <StudentView 
+    render() {
+      if (this.state.redirect)
+        return (<Redirect to="/students"/>);
+      return (
+        <StudentView 
         student={this.props.student}
+        handleClick={this.handleClick}
+        showEditClick={this.showEdit}
+        showEdit={this.state.showEdit}
+        refresh={this.refresh}
       />
     );
   }
@@ -29,6 +60,7 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
   return {
     fetchStudent: (id) => dispatch(fetchStudentThunk(id)),
+    deleteStudent:(id) => dispatch(deleteStudentThunk(id))
   };
 };
 
