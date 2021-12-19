57bfb54142972587cc2192ffe5d25a917c575ff6
GuyotDavid <48332768+GuyotDavid@users.noreply.github.com>
2021-12-18 21:28:10 -0500

    Update student.js

--- src/store/reducers/student.js
+++ src/store/reducers/student.js
@@ -8,7 +8,7 @@ const initialState = {
 const student = (state=initialState, action) => {
   switch (action.type) {
     case FETCH_STUDENT:
-      return action.payload;
+      return {state, student: action.payload};
     default:
       return state;
   }
