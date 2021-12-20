12a154d0ec916d84f88f6c2af7eaaf03861a963d
Isabel Abonitalla <isabelabonitalla@gmail.com>
2021-12-20 16:48:20 -0500

    individual campuses

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -1,11 +1,14 @@
+import { Navbar } from ".";
 
 
 const CampusView = (props) => {
   const {campus} = props;
   return (
-    <div>      
-      <h1>{campus.name}</h1>
-      <p>{campus.description}</p>
+    <div style={styles.page}>
+      <Navbar />   
+      <h1 style={styles.title}>{campus.name}</h1>
+      <p style={{textAlign: 'center'}}>{campus.description}</p>
+      <h3 style={{paddingLeft: '1vw'}}>Enrolled students:</h3>
       <ul>
       {campus.students.map( student => {
         let name = student.firstname + " " + student.lastname;
@@ -18,5 +21,17 @@ const CampusView = (props) => {
   );
 
 };
+
+const styles = {
+  page: {
+    backgroundColor: '#feeed3',
+    minHeight: '100vh',
+    color: '#865547',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: '40px'
+  },
+}
 
 export default CampusView;
