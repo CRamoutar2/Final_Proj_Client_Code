da7f21a6016290fce43936cf2384f6f741f2f898
GuyotDavid <48332768+GuyotDavid@users.noreply.github.com>
2021-12-20 02:11:33 -0500

    Update NewCampusContainer.js

--- src/components/containers/NewCampusContainer.js
+++ src/components/containers/NewCampusContainer.js
@@ -6,4 +6,18 @@ import { Redirect } from "react-router-dom";
 
 class NewCampusContainer extends Component {
 }
+
+const mapState = (state) => {
+    return {
+      allCampuses: state.allCampuses,
+    };
+  };
+
+const mapDispatch = (dispatch) => {
+    return {
+      addCampus: (newCampus) => dispatch(addCampusThunk(newCampus)),
+      fetchAllCampuses: () => dispatch(fetchAllCampusesThunk())
+    };
+  };
+
 export default connect(mapState, mapDispatch)(NewCampusContainer);
