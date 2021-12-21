57807fb1b280be7ae6142e604144fe686c3d7347
GuyotDavid <48332768+GuyotDavid@users.noreply.github.com>
2021-12-20 23:09:07 -0500

    Update EditCampusView.js

--- src/components/views/EditCampusView.js
+++ src/components/views/EditCampusView.js
@@ -1,6 +1,44 @@
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      margin: "10px"
+    }
+}));
+
 const EditCampusView = (props) => {
-    return <div>There are no campuses.</div>;
-}
-
-
+    const classes = useStyles();
+    return (
+        <div className="edit-campus">
+            <form id="edit-campus-form" onSubmit={props.handleSubmit}>
+                <TextField 
+                    className={classes.root} 
+                    variant="outlined" label="Name" name="name" 
+                    onChange={props.handleChange} value={props.campus.name} 
+                    fullWidth required/>
+                <TextField 
+                    className={classes.root} 
+                    variant="outlined" label="Address" name="address" 
+                    onChange={props.handleChange} value={props.campus.address}
+                    fullWidth required/>
+                <TextField 
+                    className={classes.root} 
+                    variant="outlined" label="Image URL" name="imageURL"
+                    onChange={props.handleChange} value={props.campus.imageURL}
+                    fullWidth />
+                <TextField
+                    className={classes.root}
+                    variant="outlined" label="Description" name="description"
+                    onChange={props.handleChange} value={props.campus.description}
+                    fullWidth/>
+                <Button 
+                    className={classes.root} type="submit"
+                    variant="contained" color="primary">Submit</Button>
+            </form>
+        </div>
+    );
+};
+  
 export default EditCampusView;
